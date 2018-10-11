d3a2a108d944cfeef1b9eb334f8324e3b24ef792
Hyunjii Cho <justcho5@gmail.com>
2018-10-11 17:52:55 +0200

    minor change to file

--- project1/implementations.py
+++ project1/implementations.py
@@ -3,16 +3,16 @@
 # kept track of all encountered w for iterative methods, here we only want the last one.
 
 
-def least_squares_GD(y, tx, initial w, max iters, gamma):
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 # Linear regression using gradient descent
-def least_squares_SGD(y, tx, initial w, max iters, gamma):
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
 # Linear regression using stochastic gradient descent
 def least_squares(y, tx):
 # Least squares regression using normal equations
 def ridge_regression(y, tx, lambda_):
 # Ridge regression using normal equations
-def logistic_regression(y, tx, initial w, max iters, gamma):
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
 # Logistic regression using gradient descent or SGD
-def reg_logistic_regression(y, tx, lambda_, initial w, max iters, gamma):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
 
 
