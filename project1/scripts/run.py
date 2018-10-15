2e05ed3a04473697b880d8bf5335a46458fbe692
Hyunjii Cho <justcho5@gmail.com>
2018-10-15 20:57:52 +0200

    move implementation file to scripts

--- project1/scripts/run.py
+++ project1/scripts/run.py
@@ -1,3 +1,11 @@
+from implementations import (
+    least_squares_GD,
+    least_squares_SGD,
+    least_squares,
+    ridge_regression,
+    logistic_regression,
+    reg_logistic_regression,
+)
 
 
 # Load the data and return y, x, and ids
@@ -7,4 +15,6 @@ y_tr, x_tr, ids_tr = load_csv_data(train_datapath)
 y_te, x_te, ids_te = load_csv_data(test_datapath)
 
 # Get the weights
-weights = least_squares_GD(y_tr, tx, initial_w, max_iters, gamma)
+# weights = least_squares_GD(y_tr, tx, initial_w, max_iters, gamma)
+weights = least_squares(y_tr, x_tr)
+print(weights)
