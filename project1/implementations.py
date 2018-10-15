07e35e18ffdcdaaf5cf31d9b4aa51afbdc08308c
Hyunjii Cho <justcho5@gmail.com>
2018-10-15 18:16:09 +0200

    helper functions in implementations

--- project1/implementations.py
+++ project1/implementations.py
@@ -1,8 +1,56 @@
 import numpy as np
+import csv
 
 # Return type: Note that all functions should return: (w, loss), which is the last weight vector of the
 # method, and the corresponding loss value (cost function). Note that while in previous labs you might have
 # kept track of all encountered w for iterative methods, here we only want the last one.
+
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+
+
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y == "b")] = -1
+
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
+
+
+def predict_labels(weights, data):
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0)] = -1
+    y_pred[np.where(y_pred > 0)] = 1
+
+    return y_pred
+
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, "w") as csvfile:
+        fieldnames = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
 
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
@@ -116,15 +164,17 @@ def ridge_regression(y, tx, lambda_):
     return (w, loss)
 
 
-
 def sigmoid(z):
-    return 1/ (1 + np.exp(-z))
+    return 1 / (1 + np.exp(-z))
+
 
 def logistic_regression_loss(y, h):
     return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
+
 def logistic_regression_gradient(x, y, h):
-    return (x.T @ (h - y )) / y.shape[0]
+    return (x.T @ (h - y)) / y.shape[0]
+
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     # Logistic regression using gradient descent or SGD
