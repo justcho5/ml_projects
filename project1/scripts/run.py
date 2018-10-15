58392183fef1ebcc4c622a2b0149068b5e50ff7b
Hyunjii Cho <justcho5@gmail.com>
2018-10-15 21:43:54 +0200

    starting the preprocessing code

--- project1/scripts/run.py
+++ project1/scripts/run.py
@@ -7,6 +7,7 @@ from implementations import (
     reg_logistic_regression,
 )
 from helpers import load_csv_data
+import numpy as np
 
 # Load the data and return y, x, and ids
 train_datapath = "../data/train.csv"
@@ -14,11 +15,20 @@ test_datapath = "../data/test.csv"
 y_tr, x_tr, ids_tr = load_csv_data(train_datapath)
 y_te, x_te, ids_te = load_csv_data(test_datapath)
 
+# Preprocessing
+# Scale each feature -1 to 1
+# -999 are nan values
+
 lambda_ = 2.27584592607e-05
+initial_w = np.random.rand(30, 1)
+print(initial_w)
+max_iters = 100
+gamma = 1 / max_iters
 # Get the weights
-# weights = least_squares_GD(y_tr, tx, initial_w, max_iters, gamma)
-ls_weights, ls_loss = least_squares(y_tr, x_tr)
-rr_weights, rr_loss = ridge_regression(y_tr, x_tr, lambda_)
+lsgd_weights, lsgd_loss = least_squares_GD(y_tr, x_tr, initial_w, max_iters, gamma)
+# ls_weights, ls_loss = least_squares(y_tr, x_tr)
+# rr_weights, rr_loss = ridge_regression(y_tr, x_tr, lambda_)
 # lr_weights, lr_loss = logistic_regression()
 # rlr_weights, rlr_loss
-print(ls_loss, rr_loss)
+print(lsgd_weights)
+print(lsgd_loss)
