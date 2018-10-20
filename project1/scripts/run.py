d9daa11eef741b8e0a40f6f8966c33650ff6d1bc
Kirusanth Poopalasingam <code@kiru.io>
2018-10-20 19:20:23 +0200

    Refactored to use Model

--- project1/scripts/run.py
+++ project1/scripts/run.py
@@ -11,8 +11,10 @@ from implementations import (
 from helpers import load_csv_data, predict_labels, create_csv_submission
 
 from cross_val import cross_validation, build_k_indices
+from multiprocessing import Pool
 
 import numpy as np
+
 
 def remove_features(x_tr, x_te, threshold=0, replace_with_mean=False):
     #     print("First five train rows before:\n", x_tr[:5], "\n")
@@ -61,7 +63,7 @@ def standardize_features(x_tr, x_te):
     return x_tr, x_te
 
 
-def predict_and_generate_file(weights, x_te):
+def predict_and_generate_file(weights, x_te, ids_te):
     print("Predict for test data")
     y_prediction = predict_labels(weights, x_te)
 
@@ -72,19 +74,16 @@ def predict_and_generate_file(weights, x_te):
 
 def read_train_test():
     # Load the data and return y, x, and ids
-    train_datapath = "../data/train.csv"
+    train_datapath = "../data/small-train.csv"
     test_datapath = "../data/test.csv"
 
     print("Load CSV file")
     y_tr, x_tr, ids_tr = load_csv_data(train_datapath, sub_sample=False)
     y_te, x_te, ids_te = load_csv_data(test_datapath)
-    #print(x_te)
+    # print(x_te)
 
-    # Preprocessing
-    print("Pre process: {} rows ".format(len(y_tr)))
-
-    # Replace -999 by NaN
-    x_tr[x_tr == -999] = 0
+    # Replace -999 by 0
+    # x_tr[x_tr == -999] = 0
 
     # x_tr : original features
     # x_tr1 : removes all features with NaNs. Resulting shape (250000, 19)
@@ -92,68 +91,115 @@ def read_train_test():
 
     x_tr1, x_te1 = remove_features(x_tr, x_te)
     x_tr2, x_te2 = remove_features(x_tr, x_te, threshold=0.7, replace_with_mean=True)
+    # x_tr3, x_te3 = np.sqrt(x_tr2, x_te2)
 
     x_tr1, x_te1 = standardize_features(x_tr1, x_te1)
     x_tr2, x_te2 = standardize_features(x_tr2, x_te2)
 
+    print("Pre process: initial {}, after cleaning {}".format(x_tr.shape, x_tr2.shape))
     x_tr = x_tr2
     x_te = x_te2
-    return x_tr, y_tr, x_te, y_te
 
-def print_loss(model_name, losses):
-    loss_tr, loss_te, avg_accuarcy = losses
-    print("Cross Validation",model_name, loss_tr, loss_te, avg_accuarcy)
+    return x_tr, y_tr, x_te, y_te, ids_te
 
-def try_different_model(x_tr, x_te, y_tr):
+
+class Model:
+    def __init__(self, name, degree):
+        self.name = name
+        self.degree = degree
+
+    def run(self, y_training, x_training, k_indices, model_function):
+        (loss_training, loss_test, accuracy) = cross_validation(y_training, x_training,
+                                                                k_indices, self.degree, model_function)
+        self.loss_training = loss_training
+        self.loss_test = loss_test
+        self.accuarcy = accuracy
+
+        # run it with the whole data set
+        extended_training_set = build_poly(x_training, self.degree)
+        (weight, total_loss) = model_function(y_training, extended_training_set)
+        self.weights = weight
+
+    def print(self):
+        print(self.degree, self.loss_training, self.loss_test, self.accuarcy, self.name)
+
+
+def try_different_models(x_training,
+                         y_training):
+    print("Try different models")
+
+    # Let's run this in a pool, so we can use all the available CPU Cores
+    # 8 ist just a number which I've chosen ( no deeper meaning )
+    with Pool(8) as pool:
+        arguments = []
+        for degree in range(1, 2):
+            arguments.append((degree, x_training, y_training))
+
+        # submit jobs to try all degrees
+        best_model_for_degree = pool.map(try_all_models_for_degree, arguments)
+
+
+        best_overall_model =  min(best_model_for_degree, key=lambda model: model.loss_test)
+        return best_overall_model
+
+
+def try_all_models_for_degree(degree_and_data):
+    degree, x_training, y_training = degree_and_data
+    print("Start Degree = ", degree)
+
     lambda_ = 2.27584592607e-05
     initial_w = np.random.rand(x_tr.shape[1], 1)
     max_iters = 100
     gamma = 1 / max_iters
 
-    # Get the weights
-    #lr_weights, lr_loss = logistic_regression(y_tr, x_tr, np.zeros(x_tr.shape[1]), max_iters, gamma)
-    #print("Loss Logistic Regression: ", lr_loss)
+    k_indices = build_k_indices(y_tr, 10)
 
-    #reg_lr_weights, reg_lr_loss = reg_logistic_regression(y_tr, x_tr, lambda_, np.zeros(x_tr.shape[1]), max_iters, gamma)
-    #print("Loss Reg. Logistic Regression: ", reg_lr_loss)
+    m_least_square = Model("Least Square", degree)
+    m_least_square.run(y_training, x_training, k_indices, least_squares)
 
-    #weights_ridge, loss_ridge = ridge_regression(y_tr, x_tr, lambda_)
-    #print("Loss Ridge Reg:", loss_ridge)
+    model_function = lambda y, x: logistic_regression(y, x, np.zeros(x.shape[1]), max_iters, gamma)
+    m_logistic_regression = Model("Logistic Regression", degree)
+    m_logistic_regression.run(y_training, x_training, k_indices, model_function)
 
-    #weights_ls, loss_ls = least_squares(y_tr, x_tr)
-    #print("Loss least square: ", loss_ls)
+    # model_function = lambda y, x: least_squares_GD(y, x, initial_w, max_iters, gamma)
+    # losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
+    # print_loss("Least Square GD", losses, degree)
+    # if (losses[2] > best_accuracy):
+    #    best_accuracy = losses[2]
+    #    best_weights = model_function(y_tr, build_poly(x_tr, degree))
 
-    #weights_ls_gd, loss_ls_gd = least_squares_GD(y_tr, x_tr, initial_w, max_iters, gamma)
-    #print("Loss least square GD: ", loss_ls_gd)
+    # model_function = lambda y, x: least_squares_SGD(y, x, initial_w, max_iters, gamma)
+    # losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
+    # print_loss("Least Square SGD", losses, degree)
+    # if (losses[2] > best_accuracy):
+    #    best_accuracy = losses[2]
+    #    best_weights = model_function(y_tr, build_poly(x_tr, degree))
 
-    #weights_ls_sgd, loss_ls_sgd = least_squares_SGD(y_tr, x_tr, initial_w, max_iters, gamma)
-    #print("Loss least square SGD: ", loss_ls_sgd)
+    model_function = lambda y, x: reg_logistic_regression(y, x, lambda_, np.zeros(x.shape[1]), max_iters, gamma)
+    m_reg_logistic_regression = Model("Logistic Reg Regression", degree)
+    m_reg_logistic_regression.run(y_training, x_training, k_indices, model_function)
 
-    for degree in range(1, 6):
-         print("Degree = ", degree)
+    model_function = lambda y, x: ridge_regression(y, x, lambda_)
+    m_ridge_regression = Model("Logistic Ridge Regression", degree)
+    m_ridge_regression.run(y_training, x_training, k_indices, model_function)
 
-         k_indices = build_k_indices(y_tr, 10, 1)
-         model_function = lambda y, x: least_squares(y, x)
-         losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
-         print_loss("Least Square", losses)
+    all_models = [m_least_square, m_logistic_regression, m_reg_logistic_regression, m_ridge_regression]
 
-         model_function = lambda y, x: logistic_regression(y, x, np.zeros(x.shape[1]), max_iters, gamma)
-         losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
-         print_loss("Logistic Regression", losses)
+    # After all are done, print result
+    for each_model in all_models:
+        each_model.print()
 
-         model_function = lambda y, x: reg_logistic_regression(y, x, lambda_, np.zeros(x.shape[1]), max_iters, gamma)
-         losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
-         print_loss("Logistic Reg Regression", losses)
+    # get best model based on the loss
+    best_model = min(all_models, key=lambda each: each.loss_test)
+    return best_model
 
-         model_function = lambda y, x: ridge_regression(y, x, lambda_)
-         losses = cross_validation(y_tr, x_tr, k_indices, degree, model_function)
-         print_loss("Logistic Ridge Regression", losses)
+np.random.seed(10)
+x_tr, y_tr, x_te, y_te, ids_te = read_train_test()
 
-         print("")
+best_overall_model = try_different_models(x_tr, y_tr)
+print("Best model is:")
+best_overall_model.print()
 
-     #return lr_weights
-
-x_tr, y_tr, x_te, y_te = read_train_test()
-try_different_model(x_tr, x_te, y_tr)
-#predict_and_generate_file(optimal_weiths, x_te, y_tr)
-
+print(best_overall_model.weights)
+extended_test_feature = build_poly(x_te, best_overall_model.degree)
+predict_and_generate_file(best_overall_model.weights, extended_test_feature, ids_te)
