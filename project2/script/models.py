673c0000b5b37dafc2aced32318c8af665cf1ecb
Kirusanth Poopalasingam <code@kiru.io>
2018-12-18 15:01:50 +0100

    run knn with baseline
    Signed-off-by: Kirusanth Poopalasingam <code@kiru.io>

--- project2/script/models.py
+++ project2/script/models.py
@@ -338,15 +338,15 @@ def cross_validates_one_by_one(pool, model_name,
 def grid_search():
     data = d.to_surprise_read('../data/data_surprise.csv')
 
-    print("Start grid search for KNNBasic")
+    print("Start grid search for KNNBaseline")
     crazy_param = {
-        'k': np.arange(5, 50, 5),
-        'min_k': [1, 2]
+        'k': np.arange(1, 100, 10),
     }
 
     gs = GridSearchCV(KNNBasic,
                       crazy_param,
+                      return_train_measures=True,
                       measures=['rmse'], cv=12, n_jobs=16, joblib_verbose=True)
     gs.fit(data)
-    pickle.dump(gs, open("output/GridSearch_KNNBasic.result", "wb"))
+    pickle.dump(gs, open("output/GridSearch_KNNBaseline.result", "wb"))
     return gs
