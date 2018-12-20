575714d84bfb75ddee6aaa13e3b5ad2f309cbf56
Kirusanth Poopalasingam <code@kiru.io>
2018-12-20 03:12:22 +0100

    cv=12
    Signed-off-by: Kirusanth Poopalasingam <code@kiru.io>

--- project2/script/run_all_blending.py
+++ project2/script/run_all_blending.py
@@ -61,7 +61,7 @@ def with_default_param():
         start_time = time.time()
 
         all = m.cross_validate(pool=pool,
-                               splits=5,
+                               splits=12,
                                model_to_param=model_to_param,
                                output_file_name=output_file_name,
                                data_file=FILE_NAME,
