8521aadd0815a5c933cfb6540a890072efe34107
Kirusanth Poopalasingam <code@kiru.io>
2018-12-20 03:38:19 +0100

    added usermean
    Signed-off-by: Kirusanth Poopalasingam <code@kiru.io>

--- project2/script/models.py
+++ project2/script/models.py
@@ -7,6 +7,9 @@ import pandas as pd
 import numpy as np
 import pickle
 
+import scipy
+import scipy.io
+import scipy.sparse as sp
 import surprise as s
 from surprise import accuracy
 from surprise import Dataset
@@ -32,6 +35,92 @@ from scipy.optimize import minimize
 import dataset as d
 import os
 from tqdm import tqdm
+
+
+def init_MF(train, num_features):
+    """init the parameter for matrix factorization."""
+
+    num_item, num_user = train.get_shape()
+
+    user_features = np.random.rand(num_features, num_user)
+    item_features = np.random.rand(num_features, num_item)
+
+    # start by item features.
+    item_nnz = train.getnnz(axis=1)
+    item_sum = train.sum(axis=1)
+
+    for ind in range(num_item):
+        item_features[0, ind] = item_sum[ind, 0] / item_nnz[ind]
+    return user_features, item_features
+
+def compute_error(data, user_features, item_features, nz):
+    """compute the loss (MSE) of the prediction of nonzero elements."""
+    mse = 0
+    for row, col in nz:
+        item_info = item_features[:, row]
+        user_info = user_features[:, col]
+        mse += (data[row, col] - user_info.T.dot(item_info)) ** 2
+    return np.sqrt(1.0 * mse / len(nz))
+
+class MatrixFactor:
+    def __init__(self):
+        self.name = "MatrixFactor"
+
+    def fit(self, trainset, testset, param):
+
+        """matrix factorization by SGD."""
+        # define parameters
+        gamma = 0.01
+        num_features = 20  # K in the lecture notes
+        lambda_user = 0.1
+        lambda_item = 0.7
+        num_epochs = 20  # number of full passes through the train set
+        errors = [0]
+
+        # set seed
+        np.random.seed(988)
+
+        # init matrix
+        user_features, item_features = init_MF(train, num_features)
+
+        # find the non-zero ratings indices
+        nz_row, nz_col = train.nonzero()
+        nz_train = list(zip(nz_row, nz_col))
+        nz_row, nz_col = test.nonzero()
+        nz_test = list(zip(nz_row, nz_col))
+
+        print("learn the matrix factorization using SGD...")
+        for it in range(num_epochs):
+            # shuffle the training rating indices
+            np.random.shuffle(nz_train)
+
+            # decrease step size
+            gamma /= 1.2
+
+            for d, n in nz_train:
+                # update W_d (item_features[:, d]) and Z_n (user_features[:, n])
+                item_info = item_features[:, d]
+                user_info = user_features[:, n]
+                err = train[d, n] - user_info.T.dot(item_info)
+
+                # calculate the gradient and update
+                item_features[:, d] += gamma * (err * user_info - lambda_item * item_info)
+                user_features[:, n] += gamma * (err * item_info - lambda_user * user_info)
+
+            rmse = compute_error(train, user_features, item_features, nz_train)
+            print("iter: {}, RMSE on training set: {}.".format(it, rmse))
+
+            errors.append(rmse)
+
+        # evaluate the test error
+        self.rmse = compute_error(test, user_features, item_features, nz_test)
+        print("RMSE on test data: {}.".format(rmse))
+
+    def predict(self, user, movie):
+        return self.global_mean
+
+
+
 
 class GlobalMean:
     ''''
@@ -235,6 +324,9 @@ def call_algo(i):
 
     if "MovieMean" in model_name:
         models.append(MovieMean())
+
+    if "MatrixFactor" in model_name:
+        models.append(MatrixFactor())
 
     print("Fit each model")
     progress = tqdm(models)
