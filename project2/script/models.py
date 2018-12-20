c17b8ec6ce5faf4a7769c48d1cd254d99237a288
Kirusanth Poopalasingam <code@kiru.io>
2018-12-20 10:07:19 +0100

    added usermean
    Signed-off-by: Kirusanth Poopalasingam <code@kiru.io>

--- project2/script/models.py
+++ project2/script/models.py
@@ -97,7 +97,8 @@ class MatrixFactor:
         num_features = 20  # K in the lecture notes
         lambda_user = 0.1
         lambda_item = 0.7
-        num_epochs = 20  # number of full passes through the train set
+        #num_epochs = 20  # number of full passes through the train set
+        num_epochs = 3  # number of full passes through the train set
         errors = [0]
 
         # init matrix
